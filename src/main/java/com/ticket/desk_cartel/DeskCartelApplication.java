1d3072e290974e0a1c3e5ce6a3fbab18677d417d
Neil <45957559+Fluffaro@users.noreply.github.com>
2025-03-17 08:52:09 +0800

    Springboot Initialization

---
+++ src/main/java/com/ticket/desk_cartel/DeskCartelApplication.java
@@ -0,0 +1,13 @@
+package com.ticket.desk_cartel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeskCartelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeskCartelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ticket/desk_cartel/DeskCartelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticket.desk_cartel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeskCartelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
