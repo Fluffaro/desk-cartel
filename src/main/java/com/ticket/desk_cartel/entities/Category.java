1a25f5d1dfb49c5c8c32834e658b10370242f389
pmcano1 <pcano@vertere-gs.com>
2025-03-17 16:12:24 +0800

    Add Category
    Created Entity, Repository, Service, and Controller for Category

---
+++ src/main/java/com/ticket/desk_cartel/entities/Category.java
@@ -0,0 +1,36 @@
+package com.ticket.desk_cartel.entities;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "category")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int categoryId;
+
+    @Column
+    private String name;
+
+    @Column
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "taskId")
+    @JsonBackReference("account-sentTransactions")
+    private Ticket ticket;
+
+    public Category(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+}
