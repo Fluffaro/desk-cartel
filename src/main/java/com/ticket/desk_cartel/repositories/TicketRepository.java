45a35a906007d6e3ccbd5196eaabcd3c1142280c
BCKT8 <kikoko200111@gmail.com>
2025-03-18 13:03:29 +0800

    filter (Priority is in enum form)
    Filter By Status and Category and Priority
    Priority is in enum form
    MUST BE ENTITY

--- src/main/java/com/ticket/desk_cartel/repositories/TicketRepository.java
+++ src/main/java/com/ticket/desk_cartel/repositories/TicketRepository.java
@@ -1,7 +1,27 @@
 package com.ticket.desk_cartel.repositories;
 
+import com.ticket.desk_cartel.entities.Category;
+import com.ticket.desk_cartel.entities.Priority;
+import com.ticket.desk_cartel.entities.Status;
 import com.ticket.desk_cartel.entities.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TicketRepository extends JpaRepository<Ticket, Integer> {
+import java.util.List;
+
+public interface TicketRepository extends JpaRepository<Ticket, Long> {
+
+
+    List<Ticket> findByTicketOwner_Id(Long userId);
+    List<Ticket> findByAssignedTicket_Id(Long assignedAgent);
+
+    //Filters for ticket
+    List<Ticket> findByCategoryAndPriorityAndStatus(Category category, Priority priority, Status status);
+    List<Ticket> findByCategoryAndPriority(Category category, Priority priority);
+    List<Ticket> findByCategoryAndStatus(Category category, Status status);
+    List<Ticket> findByPriorityAndStatus(Priority priority, Status status);
+    List<Ticket> findByCategory(Category category);
+    List<Ticket> findByPriority(Priority priority);
+    List<Ticket> findByStatus(Status status);
+
+
 }
